a96ab98b21c69bc18ea8fc47071f6aef309987ef
Sudeep Kumar <kumarpsudeep99@gmail.com>
2020-04-14 11:19:11 +0530

    Finishing of the function defenitions and the finishing the program

--- stats.c
+++ stats.c
@@ -19,6 +19,7 @@
  * Note: For function descriptions refer the "stats.h" file.
  *
  */
+
 #include <stdio.h>
 #include "stats.h"
 
@@ -44,37 +45,96 @@ void main() {
 
 void print_statistics(int arr[])
 {
-
+  int min,max;
+  float median,mean;
+  sort_array(arr,SIZE);
+  min=find_minimum(arr,SIZE);
+  max=find_maximum(arr,SIZE);
+  median=find_median(arr,SIZE);
+  mean=find_mean(arr,SIZE);
+  printf("\n The element with minimum value: %d \n",min);
+  printf("\n The element with maximum value: %d \n",max);
+  printf("\n The mean value of the array: %g \n",mean);
+  printf("\n The median value of the array: %g \n",median);
 }
 
 void print_array(int arr[],int length)
 {
-
+  printf("\nThe Given array: \nIndex  Value\n");
+  for(int i=0;i<length;i++)
+  {
+    printf(" [%d] , %d \n",i,arr[i]);// Printing with index and value
+  }
+  
 }
 
 float find_median(int arr[],int length)
 {
-
+  sort_array(arr,length);
+  float median;
+  if(length%2==0)
+  {
+    median=(arr[(length-1)/2]+arr[length/2])/2.0;//median for an array with even number of elements.
+  }
+  else
+  {
+    median=arr[length/2];//median for an array with odd number of elements.
+  }
+return median;
 }
 
 float find_mean(int arr[],int length)
 {
-
+  int sum=0;
+  for(int i=0;i<length;i++)
+  {
+    sum=sum+arr[i];//adding all elements of the array.
+  }
+  float mean=sum/length;//calculating the mean by dividing the sum by the total number of elements.
+return mean;
 }
 
 int find_maximum(int arr[],int length)
 {
-
+  int max=arr[0];
+  for(int i=0;i<length;i++)
+  {
+    if(arr[i]>max)
+    {
+      max=arr[i];
+    }
+  }
+return max;
 }
 
 int find_minimum(int arr[],int length)
 {
-
+  int min=arr[0];
+  for(int i=0;i<length;i++)
+  {
+    if(arr[i]<min)
+    {
+      min=arr[i];
+    }
+  }
+return min;
 }
 
 void sort_array(int arr[],int length)
 {
-
+  int temp;
+  for(int i=0;i<40;i++)
+  {
+    for(int j=i+1;j<40;j++)
+    {
+      if(arr[i]<arr[j])
+        {
+          temp=arr[i];
+	  arr[i]=arr[j];
+          arr[j]=temp;//swapping the elements if "i th" element is smaller than the "j th" element to create a sorted array.
+	}
+     }	
+   }
 }
 
 
